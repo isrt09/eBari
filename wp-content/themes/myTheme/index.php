f3589b8444872d7d6520036a0d5d1d450aa5bcac
mazed <mazedur@gmail.com>
2021-01-02 21:22:56 +0600

    Post Dynamic

--- wp-content/themes/myTheme/index.php
+++ wp-content/themes/myTheme/index.php
@@ -10,5 +10,18 @@
 	<h1><a href="<?php echo home_url(); ?>">Welcome to WEB GURU!</a></h1>
 	<h2><?php bloginfo('name'); ?></h2>
 	<h2><?php bloginfo('description'); ?></h2>
+	<hr>
+	<!-- Post Section -->
+	<?php 
+		if(have_posts()) :
+			while(have_posts()):the_post(); ?>
+			<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+			<p><?php the_content(); ?></p>
+	  <?php endwhile;
+		else :
+			echo "No";
+		endif;
+		
+	 ?>
 </body>
 </html>
