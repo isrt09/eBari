911310e4ede686955f1a4004f58d19902b0bf7f0
mazed <mazedur@gmail.com>
2021-01-04 00:36:32 +0600

    Post Meta Dynamic

--- wp-content/themes/myTheme/index.php
+++ wp-content/themes/myTheme/index.php
@@ -6,6 +6,11 @@
 				if(have_posts()) :
 					while(have_posts()):the_post();?>
 					<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+					<div class="post_meta">
+						Posted by : <span><?php the_author_posts_link(); ?></span> ,
+						Posted on : <span><?php the_time('d M, Y'); ?></span> ,
+						Posted In :<span><?php the_category(','); ?></span>
+					</div>
 					<p><?php the_content(); ?></p>	
 			<?php   endwhile;
 				else:
