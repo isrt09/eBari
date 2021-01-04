038540d3cf1c57d7cd407985f24a491395dc5ccd
mazed <mazedur@gmail.com>
2021-01-04 13:12:57 +0600

    404 Page added

---
+++ wp-content/themes/myTheme/404.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>   
+	<div class="content_wrapper">
+		<div class="left_content">
+			<h2>Sorry! Your are wrong way to find out! Please Try Again..</h2>			
+		</div>		
+		<br class="clear" />
+	</div>	
+<?php get_footer(); ?>	
